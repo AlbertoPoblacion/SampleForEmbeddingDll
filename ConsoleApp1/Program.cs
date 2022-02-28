fff912b70a3f41a2facc60886f7fd06c21219047
Alberto Poblacion <earthling@poblacion.org>
2022-02-28 16:26:35 +0000

    Initial skeleton, no embedding

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var library = new ClassLibrary1.Class1();
+            string text = library.GetSomeText();
+            Console.WriteLine(text);
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.WriteLine("\nPress ENTER to exit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
