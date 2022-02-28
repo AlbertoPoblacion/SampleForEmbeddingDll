560758ae483754808d58b1c8fddb8dbb041f7017
Alberto Poblacion <earthling@poblacion.org>
2022-02-28 16:42:41 +0000

    Added embedded DLL and code to load it.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,15 +11,36 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var library = new ClassLibrary1.Class1();
-            string text = library.GetSomeText();
-            Console.WriteLine(text);
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+
+            DoSomething();
 
             if (System.Diagnostics.Debugger.IsAttached)
             {
                 Console.WriteLine("\nPress ENTER to exit");
                 Console.ReadLine();
             }
+        }
+
+        private static void DoSomething()
+        {
+            // This has to be outside Main !!! (think about the load order when JITting Main)
+            var library = new ClassLibrary1.Class1();
+            string text = library.GetSomeText();
+            Console.WriteLine(text);
+        }
+
+        private static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            string assemblyThatWeAreResolving = args.Name;
+            if (assemblyThatWeAreResolving.ToLower().Contains("classlibrary1"))
+            {
+                byte[] b = EmbeddedLibraries.ClassLibrary1;
+                Assembly a = Assembly.Load(b);
+                return a;
+            }
+
+            return null;
         }
     }
 }
